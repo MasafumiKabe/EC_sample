fe639f14329c3667b3c7b4ae58e36f5dd1b14373
MasafumiKabe <msfmkb@gmail.com>
2019-07-02 03:42:44 +0900

    user registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def require_user_logged_in
+    unless user_signed_in?
+      redirect_to root_url
+    end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'toppages/index'
 
   root to: 'toppages#index'
+  resources :users, only: [:show]
 end
